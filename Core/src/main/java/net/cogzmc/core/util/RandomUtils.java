c0eb7052acdb2792832f62e6d0312c0251f2153c
Jake <jake0oo0andminecraft@gmail.com>
2014-05-22 22:18:26 -0500

    Start factions

--- Core/src/main/java/net/cogzmc/core/util/RandomUtils.java
+++ Core/src/main/java/net/cogzmc/core/util/RandomUtils.java
@@ -2,6 +2,7 @@ package net.cogzmc.core.util;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.List;
 
 public final class RandomUtils {
     public static void setDeclaredField(Object value, Object instance, String field) throws NoSuchFieldException, IllegalAccessException {
@@ -47,5 +48,19 @@ public final class RandomUtils {
             if (ts[x].equals(t)) return x;
         }
         return -1;
+    }
+
+    public static <T> String formatList(List<? extends T> list, int max) {
+        StringBuilder builder = new StringBuilder();
+        int current = 0;
+        for (T aValue : list) {
+            if (current == max) break;
+            builder.append(aValue).append(", ");
+            current++;
+        }
+        if (list.size() > 0) {
+            builder.deleteCharAt(builder.length() - 2);
+        }
+        return builder.toString();
     }
 }
