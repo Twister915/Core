bd5bf3609ffb51b4fb17d03a1bf5ce6f0f62ebe7
Joey Sacchini <joey.sacchini264@gmail.com>
2014-05-29 16:18:47 -0400

    Thanks @NoyHillel @georgepadolsey for that.

--- Core/src/main/java/net/cogzmc/core/player/CooldownManager.java
+++ Core/src/main/java/net/cogzmc/core/player/CooldownManager.java
@@ -21,12 +21,12 @@ public final class CooldownManager {
         //See how long ago that was in milliseconds
         long millisecondsPassed = currentDate.getTime() - lastFiredDate.getTime();
         //And see how long we're supposed to wait
-        long milliseconds = unit.convert(time, TimeUnit.MILLISECONDS);
+        long milliseconds = unit.toMillis(time);
         //If we're supposed to wait longer than we have
         if (milliseconds >= millisecondsPassed) {
             //The cooldown has yet to expire
             if (reset) this.cooldownMilliseconds.put(key, currentDate);
-            throw new CooldownUnexpiredException(unit.convert(milliseconds-millisecondsPassed, TimeUnit.MILLISECONDS), unit);
+            throw new CooldownUnexpiredException(unit.toMillis(milliseconds-millisecondsPassed), unit);
         }
         this.cooldownMilliseconds.put(key, currentDate);
     }
