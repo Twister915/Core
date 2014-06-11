76a6c2f4e1a4e88415cf64c72ba95ee4eeef28e7
Joe Hirschfeld <joe@ibj.io>
2014-06-11 19:25:14 -0400

    Added clean module exists check to core

--- Core/src/main/java/net/cogzmc/core/netfiles/mongo/MongoNetFileManager.java
+++ Core/src/main/java/net/cogzmc/core/netfiles/mongo/MongoNetFileManager.java
@@ -1,7 +1,11 @@
 package net.cogzmc.core.netfiles.mongo;
 
+import com.mongodb.DB;
 import net.cogzmc.core.netfiles.NetFileManager;
 
 //${user}'s code heheu
 public class MongoNetFileManager extends MongoNetDirectory implements NetFileManager {
+    public MongoNetFileManager(DB db) {
+        super(db);
+    }
 }
