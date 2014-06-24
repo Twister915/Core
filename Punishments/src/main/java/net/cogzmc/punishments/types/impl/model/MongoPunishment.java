b5be3fa8a5de8f22ccd0a2d327e63df1b500ac74
Joey Sacchini <joey.sacchini264@gmail.com>
2014-06-24 05:58:16 -0400

    No NPE!

--- Punishments/src/main/java/net/cogzmc/punishments/types/impl/model/MongoPunishment.java
+++ Punishments/src/main/java/net/cogzmc/punishments/types/impl/model/MongoPunishment.java
@@ -1,5 +1,6 @@
 package net.cogzmc.punishments.types.impl.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
@@ -10,9 +11,10 @@ import org.bson.types.ObjectId;
 import java.util.Date;
 
 @EqualsAndHashCode
+@AllArgsConstructor
 @Data
 public abstract class MongoPunishment implements Punishment {
-    @NonNull private ObjectId mongoId;
+    private ObjectId mongoId;
     private final COfflinePlayer target;
     private final String message;
     private final COfflinePlayer issuer;
