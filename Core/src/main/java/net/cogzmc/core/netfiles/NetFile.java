0134ff55ff93b489c2b84a89a248052bdab9b64c
Joe Hirschfeld <joe@ibj.io>
2014-06-27 10:11:42 -0400

    Forced local file copy to throw a IOException, since all Disk Operations throw an IO exception

--- Core/src/main/java/net/cogzmc/core/netfiles/NetFile.java
+++ Core/src/main/java/net/cogzmc/core/netfiles/NetFile.java
@@ -1,7 +1,8 @@
 package net.cogzmc.core.netfiles;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface NetFile extends NetElement{
-    void copyToLocalFile(File file);
+    void copyToLocalFile(File file) throws IOException;
 }
