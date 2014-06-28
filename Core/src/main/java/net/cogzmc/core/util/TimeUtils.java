d1fc249053c06d94fa5ce14644e56db6b39cd194
Joey Sacchini <joey.sacchini264@gmail.com>
2014-06-28 17:30:10 -0400

    Some comments for the hard to read code, also made it case insensitive

--- Core/src/main/java/net/cogzmc/core/util/TimeUtils.java
+++ Core/src/main/java/net/cogzmc/core/util/TimeUtils.java
@@ -16,25 +16,27 @@ public class TimeUtils {
     }
 
     public static Double parseTime(String arg){
+        //Get all the characters, example string 1d2h35m17s
         char[] chars = arg.toCharArray();
-        Double seconds = 0.0;
-        for (int x = 0; x < chars.length; x++) {
-            if (!TIMEMAP.containsKey(chars[x])) continue;
-            Double multiplier = TIMEMAP.get(chars[x]);
-            StringBuilder number = new StringBuilder();
-            for (int cursor = x-1; cursor >= 0; cursor--) {
-                char aChar = chars[cursor];
-                if (!Character.isDigit(cursor)) break;
-                number.append(aChar);
+        Double seconds = 0.0; //We start with zero seconds
+        for (int x = 0; x < chars.length; x++) { //Will check each char
+            char characterTesting = Character.toLowerCase(chars[x]);
+            if (!TIMEMAP.containsKey(characterTesting)) continue; //This will pass for each of the time markers
+            Double multiplier = TIMEMAP.get(characterTesting); //Gets the multiplexer for this marker, for "d" it will be 86400
+            StringBuilder number = new StringBuilder(); //Start to build all the characters before this marker
+            for (int cursor = x-1; cursor >= 0; cursor--) { //Go through it from the previous character until it reaches the start of our string going in reverse
+                char aChar = chars[cursor]; //Gets the current character
+                if (!Character.isDigit(cursor)) break; //If we've reached the end of the digits, stop reading
+                number.append(aChar); //otherwise, append it to the string builder
             }
-            String s = number.reverse().toString();
-            Double time;
+            String s = number.reverse().toString(); //Then, reverse it and get the string value
+            Double time; //Attempt a parse
             try {
                 time = Double.valueOf(s);
             } catch (NumberFormatException e) {
                 continue;
             }
-            seconds += time*multiplier;
+            seconds += time*multiplier; //And add it onto the total seconds
         }
         return seconds;
     }
