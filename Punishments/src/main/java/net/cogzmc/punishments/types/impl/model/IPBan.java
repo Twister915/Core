2fd29ac2a8228dcb65c400738c9f027c7ffb005f
Joey Sacchini <joey.sacchini264@gmail.com>
2014-07-03 00:25:41 -0400

    Have an equals and hashcode.

--- Punishments/src/main/java/net/cogzmc/punishments/types/impl/model/IPBan.java
+++ Punishments/src/main/java/net/cogzmc/punishments/types/impl/model/IPBan.java
@@ -1,10 +1,12 @@
 package net.cogzmc.punishments.types.impl.model;
 
+import lombok.EqualsAndHashCode;
 import net.cogzmc.core.player.COfflinePlayer;
 import org.bson.types.ObjectId;
 
 import java.util.Date;
 
+@EqualsAndHashCode(callSuper = true)
 public final class IPBan extends MongoPunishment {
     public IPBan(ObjectId mongoId, COfflinePlayer target, String message, COfflinePlayer issuer, boolean active, Date dateIssued) {
         super(mongoId, target, message, issuer, active, dateIssued);
