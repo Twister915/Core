925a08ac4b46e2e3eb02299fbef08265e228d861
August <augustt198@gmail.com>
2014-08-23 00:42:53 -0400

    Add parser regular expressions.

---
+++ Core/src/main/java/net/cogzmc/core/player/message/parser/ParserRegex.java
@@ -0,0 +1,29 @@
+package net.cogzmc.core.player.message.parser;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by August on 8/22/14.
+ */
+public enum ParserRegex {
+
+	EMPHASIS("\\*\\*((?!\\s).+(?<!\\s))\\*\\*"),
+	ITALIC("\\*((?:[^ \\*]).+(?:[^ \\*]))\\*"),
+	UNDERLINE("__((?!\\s).+(?<!\\s))__"),
+	STRIKETHROUGH("--(?!\\s)(.+)(?<!\\s)--"),
+	LINK("\\[(.+)\\]\\((.+)\\)"),
+	COMMAND("\\{\\/(.+)\\}\\((.+)\\)"),
+	SUGGEST_COMMAND("\\{\\*\\/(.+)\\*\\}\\((.+)\\)"),
+	HOVER("\\[\\[(.+)\\]\\]\\((.+)\\)");
+
+	private Pattern pattern;
+
+	ParserRegex(String regex) {
+		this.pattern = Pattern.compile(regex);
+	}
+
+	public Pattern getPattern() {
+		return pattern;
+	}
+
+}
