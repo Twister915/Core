7c0ab816e25a1392b1812f7b71b23cf6547b082d
Joey Sacchini <joey.sacchini264@gmail.com>
2014-08-29 02:20:45 -0400

    Removed un-needed package-info.java

--- Core/src/main/java/net/cogzmc/core/player/mongo/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The classes that are marked with <b>live</b> are ones that exhibit behaviour only necessary in a live (mostly Bukkit)
- * environment. Classes in the module {@code CoreBase} are all independent of Bukkit and do not need certain behavior to
- * be implemented.
- *
- * An example of this behavior is the {@link net.cogzmc.core.player.mongo.COfflineMongoPlayer} versus {@link net.cogzmc.core.player.mongo.COfflineLiveMongoPlayer}
- *  you can see that the <b>live</b> class registers as a {@link net.cogzmc.core.player.mongo.GroupReloadObserver} whereas the regular class does not.
- */
-package net.cogzmc.core.player.mongo;
