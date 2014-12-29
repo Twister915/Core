489b321e39dae85d12fc370e1b278ba10cf1b624
Twister915 <joey.sacchini264@gmail.com>
2014-12-28 22:39:07 -0500

    Updated this to be more applicable to real use

--- Core/src/main/java/net/cogzmc/core/player/CooldownUnexpiredException.java
+++ Core/src/main/java/net/cogzmc/core/player/CooldownUnexpiredException.java
@@ -2,12 +2,19 @@ package net.cogzmc.core.player;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import net.cogzmc.core.modular.command.CommandException;
 
 import java.util.concurrent.TimeUnit;
 
 @EqualsAndHashCode(callSuper = false)
 @Data
-public final class CooldownUnexpiredException extends Exception {
+public final class CooldownUnexpiredException extends CommandException {
     private final Long timeRemaining;
     private final TimeUnit timeUnit;
+
+    public CooldownUnexpiredException(Long timeRemaining, TimeUnit timeUnit) {
+        super("Unexpired cooldown");
+        this.timeRemaining = timeRemaining;
+        this.timeUnit = timeUnit;
+    }
 }
